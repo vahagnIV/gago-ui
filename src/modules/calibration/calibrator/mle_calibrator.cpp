14b95a051651e4ae44ed1710435860ef75979e3d
Vahagn IV <vahagn@nvision.am>
2020-01-25 00:22:11 +0400

    Added calibrator draft

--- src/modules/calibration/calibrator/mle_calibrator.cpp
+++ src/modules/calibration/calibrator/mle_calibrator.cpp
@@ -5,25 +5,27 @@
 #include "mle_calibrator.h"
 #include "ui_mle_calibration_window.h"
 #include "common/video_player.h"
+#include "thumb_show_item_model.h"
 #include <QHBoxLayout>
 #include <chrono>
-#include <QtCore/QDir>
-#include <QtGui/QImageWriter>
+#include <QDir>
+#include <QImageWriter>
 
 namespace gago {
 namespace gui {
 namespace calibration {
 
 MLECalibrator::MLECalibrator(QWidget *parent,
-                             const std::shared_ptr<gago::calibration::pattern::IPattern> & pattern,
-                             const MLEConfigurationSettings & settings) : QDialog(parent),
-                                                                          ui_(new Ui::MLECalibrationWindow()),
-                                                                          pattern_(pattern),
-                                                                          settings_(settings),
-                                                                          last_image_index(0),
-                                                                          next_capture_time_(std::numeric_limits<typeof(next_capture_time_)>::max()) {
+                             const std::shared_ptr<gago::calibration::pattern::IPattern> &pattern,
+                             const MLEConfigurationSettings &settings) : QDialog(parent),
+                                                                         ui_(new Ui::MLECalibrationWindow()),
+                                                                         pattern_(pattern),
+                                                                         settings_(settings),
+                                                                         last_image_index(0),
+                                                                         next_capture_time_(std::numeric_limits<typeof(next_capture_time_)>::max()) {
   ui_->setupUi(this);
   connect(ui_->pushButton, &QPushButton::pressed, this, &MLECalibrator::CaptureRequested);
+  ui_->listView->setAutoFillBackground(true);
 
 }
 
@@ -39,19 +41,19 @@ void MLECalibrator::Calibrate() {
   exec();
 }
 
-void MLECalibrator::Notify(const std::shared_ptr<std::vector<io::video::Capture>> & ptr) {
+void MLECalibrator::Notify(const std::shared_ptr<std::vector<io::video::Capture>> &ptr) {
   std::vector<cv::Mat> images;
   for (int j = 0; j < ptr->size(); ++j) {
     images.push_back((*ptr)[j].data);
   }
   std::vector<std::vector<cv::Point2f>> pts;
   pattern_->Extract(images, pts);
-  if (!std::all_of(pts.begin(), pts.end(), [](const std::vector<cv::Point2f> & item) { return item.size(); })) {
+  if (!std::all_of(pts.begin(), pts.end(), [](const std::vector<cv::Point2f> &item) { return item.size(); })) {
     long now = std::chrono::high_resolution_clock::now().time_since_epoch().count();
     if (next_capture_time_ < now) {
       next_capture_time_ = std::numeric_limits<typeof(next_capture_time_)>::max();
       char buffer[512];
-      for (const io::video::Capture & capture: *ptr) {
+      for (const io::video::Capture &capture: *ptr) {
 
         QString filename = QString::asprintf(format, capture.camera->GetName().c_str(), last_image_index);
         QDir dir(settings_.image_save_folder);
@@ -62,8 +64,8 @@ void MLECalibrator::Notify(const std::shared_ptr<std::vector<io::video::Capture>
                      QImage::Format_RGB888);
         QImageWriter writer(dir.filePath(filename));
         writer.write(image);
-
       }
+      ui_->pushButton->setEnabled(true);
       ++last_image_index;
     }
   }
@@ -75,18 +77,22 @@ void MLECalibrator::Notify(const std::shared_ptr<std::vector<io::video::Capture>
 
 }
 
-void MLECalibrator::SetCameras(const std::vector<const io::video::CameraMeta *> & vector) {
+void MLECalibrator::SetCameras(const std::vector<const io::video::CameraMeta *> &vector) {
 
   QStringList cam_names;
   for (int i = 0; i < vector.size(); ++i) {
     cam_names.append(vector[i]->GetName().c_str());
     players_.push_back(new common::VideoPlayer());
     this->ui_->horizontalLayout_3->addWidget(players_[i]);
+    //this->ui_->horizontalLayout_3->addStretch(1);
+
   }
-  //ui_->listView->setModel(new Thum)
+
+  ui_->listView->setModel(new ThumbShowItemModel(QDir(settings_.image_save_folder), cam_names));
 }
 
 void MLECalibrator::CaptureRequested() {
+  ui_->pushButton->setEnabled(false);
   next_capture_time_ = (std::chrono::high_resolution_clock::now().time_since_epoch()
       + std::chrono::seconds(settings_.wait_time)).count();
 }
--- src/modules/calibration/calibrator/thumb_show_item_model.cpp
+++ src/modules/calibration/calibrator/thumb_show_item_model.cpp
@@ -1,28 +1,57 @@
 #include "thumb_show_item_model.h"
 #include <QPixmap>
+#include <QtGui/QPainter>
 namespace gago {
 namespace gui {
 namespace calibration {
 
-ThumbShowItemModel::ThumbShowItemModel(const QDir & directory, const QStringList & cameras, QObject *parent)
+ThumbShowItemModel::ThumbShowItemModel(const QDir &directory, const QStringList &cameras, QObject *parent)
     : QAbstractItemModel(parent),
       cameras_(cameras),
       directory_(directory) {
   directory_.setSorting(QDir::Name);
-  for (int i = 0; i < cameras.size(); ++i) {
+
+  /*for (int i = 0; i < cameras.size(); ++i) {
     cameras_[i] = cameras_[i] + "_*.jpg";
-  }
-  QStringList l = directory_.entryList({cameras_[0]}, QDir::NoFilter, QDir::Name);
-  for (int j = 0; j < l.count(); ++j) {
-    int index = l[j].right(7).left(3).toInt();
+  }*/
+  QStringList filter = {cameras[0] + "_*.jpg"};
+  QStringList first_camera_files = directory_.entryList(filter, QDir::NoFilter, QDir::Name);
+
+  for (int j = 0; j < first_camera_files.size(); ++j) {
+
+    int index = first_camera_files[j].right(7).left(3).toInt();
+    QStringList filenames = {first_camera_files[j]};
+
     for (int i = 1; i < cameras.size(); ++i) {
-      bool all_exist = true;
-      if (QFile::exists(directory_.filePath(cameras[i]))) {
-        all_exist = false;
-        break;
+      QString filename = QString("%1_%2.jpg").arg(cameras[i]).arg(QString::number(index).rightJustified(3, '0'));
+      if (QFile::exists(directory_.filePath(filename))) {
+        filenames.append(filename);
       }
     }
-    //if(all_exi)
+    if (filenames.count() != cameras.size())
+      continue;
+
+    QList<QImage> images;
+    int total_length = 0;
+    int max_height = 0;
+    for (int k = 0; k < filenames.size(); ++k) {
+      QImage image(directory.filePath(filenames[k]));
+      total_length += image.width();
+      max_height = std::max(max_height, image.height());
+      images.append(image);
+    }
+
+    QImage thumbnail(total_length, max_height, QImage::Format_RGB888);
+    QPainter painter;
+    int offset = 0;
+    painter.begin(&thumbnail);
+    for (const QImage &image: images) {
+      painter.drawImage(offset, 0, image);
+      offset += image.width();
+    }
+    painter.end();
+
+    images_.append(thumbnail.scaledToWidth(150, Qt::TransformationMode::SmoothTransformation));
   }
 }
 
@@ -30,29 +59,30 @@ QVariant ThumbShowItemModel::headerData(int section, Qt::Orientation orientation
   // FIXME: Implement me!
 }
 
-QModelIndex ThumbShowItemModel::index(int row, int column, const QModelIndex & parent) const {
+QModelIndex ThumbShowItemModel::index(int row, int column, const QModelIndex &parent) const {
+  return createIndex(row, column);
   // FIXME: Implement me!
 }
 
-QModelIndex ThumbShowItemModel::parent(const QModelIndex & index) const {
+QModelIndex ThumbShowItemModel::parent(const QModelIndex &index) const {
   return QModelIndex();
   // FIXME: Implement me!
 }
 
-int ThumbShowItemModel::rowCount(const QModelIndex & parent) const {
-  if (!parent.isValid())
-    return 0;
+int ThumbShowItemModel::rowCount(const QModelIndex &parent) const {
+  /*if (!parent.isValid())
+    return 0;*/
   return images_.count();
 }
 
-int ThumbShowItemModel::columnCount(const QModelIndex & parent) const {
-  if (!parent.isValid())
-    return 0;
+int ThumbShowItemModel::columnCount(const QModelIndex &parent) const {
+  /*if (!parent.isValid())
+    return 0;*/
   return 1;
   // FIXME: Implement me!
 }
 
-QVariant ThumbShowItemModel::data(const QModelIndex & index, int role) const {
+QVariant ThumbShowItemModel::data(const QModelIndex &index, int role) const {
   if (!index.isValid())
     return QVariant();
   if (role == Qt::DecorationRole) {
